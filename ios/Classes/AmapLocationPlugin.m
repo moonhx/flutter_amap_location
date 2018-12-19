b1637569976e32e003d9ad617771b8a1187dc15c
xzhgan <xzhgan@cn.ibm.com>
2018-12-19 20:17:48 +0800

    gcj02ToWgs84

--- ios/Classes/AmapLocationPlugin.m
+++ ios/Classes/AmapLocationPlugin.m
@@ -2,6 +2,7 @@
 
 #import <AMapFoundationKit/AMapFoundationKit.h>
 #import <AMapLocationKit/AMapLocationKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 
 /*
@@ -12,6 +13,26 @@ static NSDictionary* DesiredAccuracy = @{@"kCLLocationAccuracyBest":@(kCLLocatio
                                          @"kCLLocationAccuracyThreeKilometers":@(kCLLocationAccuracyThreeKilometers),
                                          
                                          };*/
+#define LAT_OFFSET_0(x,y) -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(fabs(x))
+#define LAT_OFFSET_1 (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0
+#define LAT_OFFSET_2 (20.0 * sin(y * M_PI) + 40.0 * sin(y / 3.0 * M_PI)) * 2.0 / 3.0
+#define LAT_OFFSET_3 (160.0 * sin(y / 12.0 * M_PI) + 320 * sin(y * M_PI / 30.0)) * 2.0 / 3.0
+
+#define LON_OFFSET_0(x,y) 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(fabs(x))
+#define LON_OFFSET_1 (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0
+#define LON_OFFSET_2 (20.0 * sin(x * M_PI) + 40.0 * sin(x / 3.0 * M_PI)) * 2.0 / 3.0
+#define LON_OFFSET_3 (150.0 * sin(x / 12.0 * M_PI) + 300.0 * sin(x / 30.0 * M_PI)) * 2.0 / 3.0
+
+
+#define RANGE_LON_MAX 137.8347
+#define RANGE_LON_MIN 72.004
+#define RANGE_LAT_MAX 55.8271
+#define RANGE_LAT_MIN 0.8293
+// jzA = 6378245.0, 1/f = 298.3
+// b = a * (1 - f)
+// ee = (a^2 - b^2) / a^2;
+#define jzA 6378245.0
+#define jzEE 0.00669342162296594323
 
 
 @interface AmapLocationPlugin()<AMapLocationManagerDelegate>
@@ -23,6 +44,60 @@ static NSDictionary* DesiredAccuracy = @{@"kCLLocationAccuracyBest":@(kCLLocatio
 @end
 
 @implementation AmapLocationPlugin
+
++ (double)transformLat:(double)x bdLon:(double)y
+{
+    double ret = LAT_OFFSET_0(x, y);
+    ret += LAT_OFFSET_1;
+    ret += LAT_OFFSET_2;
+    ret += LAT_OFFSET_3;
+    return ret;
+}
+
++ (double)transformLon:(double)x bdLon:(double)y
+{
+    double ret = LON_OFFSET_0(x, y);
+    ret += LON_OFFSET_1;
+    ret += LON_OFFSET_2;
+    ret += LON_OFFSET_3;
+    return ret;
+}
+
++ (CLLocationCoordinate2D)gcj02Encrypt:(double)ggLat bdLon:(double)ggLon
+{
+    CLLocationCoordinate2D resPoint;
+    double mgLat;
+    double mgLon;
+    double dLat = [self transformLat:(ggLon - 105.0)bdLon:(ggLat - 35.0)];
+    double dLon = [self transformLon:(ggLon - 105.0) bdLon:(ggLat - 35.0)];
+    double radLat = ggLat / 180.0 * M_PI;
+    double magic = sin(radLat);
+    magic = 1 - jzEE * magic * magic;
+    double sqrtMagic = sqrt(magic);
+    dLat = (dLat * 180.0) / ((jzA * (1 - jzEE)) / (magic * sqrtMagic) * M_PI);
+    dLon = (dLon * 180.0) / (jzA / sqrtMagic * cos(radLat) * M_PI);
+    mgLat = ggLat + dLat;
+    mgLon = ggLon + dLon;
+    
+    resPoint.latitude = mgLat;
+    resPoint.longitude = mgLon;
+    return resPoint;
+}
+
++ (CLLocationCoordinate2D)gcj02Decrypt:(double)gjLat gjLon:(double)gjLon {
+    CLLocationCoordinate2D  gPt = [self gcj02Encrypt:gjLat bdLon:gjLon];
+    double dLon = gPt.longitude - gjLon;
+    double dLat = gPt.latitude - gjLat;
+    CLLocationCoordinate2D pt;
+    pt.latitude = gjLat - dLat;
+    pt.longitude = gjLon - dLon;
+    return pt;
+}
+
++ (CLLocationCoordinate2D)gcj02ToWgs84:(CLLocationCoordinate2D)location
+{
+    return [self gcj02Decrypt:location.latitude gjLon:location.longitude];
+}
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel
@@ -213,8 +288,12 @@ static NSDictionary* DesiredAccuracy = @{@"kCLLocationAccuracyBest":@(kCLLocatio
 
 +(NSDictionary*)location2map:(CLLocation *)location{
     
-    return @{@"latitude": @(location.coordinate.latitude),
-             @"longitude": @(location.coordinate.longitude),
+    CLLocationCoordinate2D pt;
+    pt.latitude =location.coordinate.latitude;
+    pt.longitude =location.coordinate.longitude;
+    CLLocationCoordinate2D  wpt = [self gcj02ToWgs84:pt];
+    return @{@"latitude": @(wpt.latitude),
+             @"longitude": @(wpt.longitude),
              @"accuracy": @((location.horizontalAccuracy + location.verticalAccuracy)/2),
              @"altitude": @(location.altitude),
              @"speed": @(location.speed),
